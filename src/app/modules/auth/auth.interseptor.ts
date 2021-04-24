eead351b34a97f5799ebddb86d527b6fbaaab582
dawood-yousef92 <dawood.y@growthway.net>
2021-04-25 02:27:31 +0300

    25/4

--- src/app/modules/auth/auth.interseptor.ts
+++ src/app/modules/auth/auth.interseptor.ts
@@ -1,15 +1,15 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
-import {Observable, throwError} from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: "root",
+  providedIn: "root",
 })
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private toaster: ToastrService) {}
+  constructor(private toaster: ToastrService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -33,47 +33,47 @@ export class AuthInterceptor implements HttpInterceptor {
       if (token != null) {
         request = request.clone({
           setHeaders: {
-              Authorization: 'Bearer ' + token.replace(/\"/g, ""),
-            // "X-Tenant": tanent,
-            "X-Tenant": 'hunter_allison_associates-',
+            Authorization: 'Bearer ' + token.replace(/\"/g, ""),
+            "X-Tenant": tanent,
+            // "X-Tenant": 'reese_and_vaughn_traders-',
           },
         });
       }
       else {
         request = request.clone({
           setHeaders: {
-            // "X-Tenant": tanent,
-            "X-Tenant": 'hunter_allison_associates-',
+            "X-Tenant": tanent,
+            // "X-Tenant": 'reese_and_vaughn_traders-',
           },
         });
       }
     }
 
     return next.handle(request)
-    .pipe(catchError(err => {
+      .pipe(catchError(err => {
         if ([401, 403].includes(err.status)) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('permissions');
-            localStorage.removeItem('adminLogin');
-            window.location.href = ('auth/login');
+          localStorage.removeItem('token');
+          localStorage.removeItem('permissions');
+          localStorage.removeItem('adminLogin');
+          window.location.href = ('auth/login');
         }
 
-        if(err.error.validationErrors) {
+        if (err.error.validationErrors) {
           const error = err.error.validationErrors;
           error.map((item) => {
-              this.toaster.error(item.reason);
+            this.toaster.error(item.reason);
           })
           return throwError(error);
         }
-        else if(err.error.title) {
+        else if (err.error.title) {
           const error = err.error;
           this.toaster.error(err.error.title)
           return throwError(error);
         }
-        else if(err.error.detail) {
-            const error = err.error;
-            this.toaster.error(err.error.detail)
-            return throwError(error);
+        else if (err.error.detail) {
+          const error = err.error;
+          this.toaster.error(err.error.detail)
+          return throwError(error);
         }
         else {
           const error = err.error;
@@ -92,7 +92,7 @@ export class AuthInterceptor implements HttpInterceptor {
         //   const error =  err.statusText;
         //   return throwError(error);
         // }
-    }));
+      }));
   }
 
 }
